a70d1acef8dcedcc233bfa8ebfb8a2f028ab024a
Kailey Liou <kaileyliou09@gmail.com>
2025-11-15 18:29:58 -0500

    added portal

--- area_2d.gd
+++ area_2d.gd
@@ -1,5 +1,5 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
+	if body.is_in_group("player2"):
 		get_tree().change_scene_to_file("res://Level2.tscn")
